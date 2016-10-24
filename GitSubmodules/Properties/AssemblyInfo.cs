4faadfdc8ab7d7a6ebdb34cce7f21a4bdadcfa44
Tobias Sekan <tobias.sekan@gmail.com>
2016-10-24 19:46:35 +0200

    Updated version number and VSIX for testers

--- GitSubmodules/Properties/AssemblyInfo.cs
+++ GitSubmodules/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GitSubmodules")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Visual Studio Plugin for Git submodules")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Company")]
 [assembly: AssemblyProduct("GitSubmodules")]
@@ -28,8 +28,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.4.0.0")]
-[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
 
 
 
