9afa58c419219a064faadd50ba485a6f82edf066
Tobias Sekan <tobias.sekan@gmail.com>
2016-10-31 21:44:15 +0100

    Added missing PublicKey for UnitTest project

--- GitSubmodules/Properties/AssemblyInfo.cs
+++ GitSubmodules/Properties/AssemblyInfo.cs
@@ -12,9 +12,18 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
+
 [assembly: NeutralResourcesLanguage("en-US")]
+
 [assembly: AssemblyVersion("1.5.0.0")]
 [assembly: AssemblyFileVersion("1.5.0.0")]
-[assembly: InternalsVisibleTo("GitSubmodules.UnitTest")]
+
+[assembly: InternalsVisibleTo("GitSubmodules.UnitTest, PublicKey="
+    + "00240000048000009400000006020000002400005253413100040000010001006f5988a51ad305"
+    + "5b275435a570c3f051d93cd00f02dbe4ff64c45349e1fe5a2349bd0ebf250a14ce37c4471572e6"
+    + "830ffe7310b163211f7549164d0a20f33ea6d7f9d7bfb4581e253478c137755db63d601dea3fbf"
+    + "ad400b99129fe1245ff549287084e50b528260958d878b3a68bce9caf66c40ad05ec26948d849e"
+    + "72b851d3")]
